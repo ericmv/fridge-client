30e8a8cd13ea2875348190f88db0135cf90d14d1
ericmv <ericmv@uci.edu>
2018-09-17 21:00:10 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -7,35 +7,13 @@
  */
 
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
-import HomeScreen from './app/components/HomeScreen/HomeScreen';
-import BrowseHome from './app/components/Browse/BrowseHome';
-import BrowseCategory from './app/components/Browse/BrowseCategory';
-import Results from './app/components/Results/Results';
-import Login from './app/components/Validate/Login';
-import AddItem from './app/components/Add/AddItem'
-
-
-import {createStackNavigator} from 'react-navigation';
-
-const RootStack = createStackNavigator(
-  {
-    Home:{screen: HomeScreen},
-    Browse:{screen: BrowseHome},
-    BrowseCategory: {screen: BrowseCategory},
-    Login: {screen: Login},
-    Results: {screen: Results},
-    Add: {screen: AddItem}
-  },
-  {
-    initialRouteName: 'Add',
-  }
-);
+import {AppRegistry, StyleSheet, Text, View, Image} from 'react-native';
+import TabNavigator from './app/components/Navigation/Navigation'
 
 export default class fridge extends Component {
   render() {
     return (
-      <RootStack />
+      <TabNavigator />
     );
   }
 }
